99b7bd17f4a4c104b521b151401fa26ca0f95358
ZARA <ZLEVENTHAL@GMAIL.COM>
2016-05-20 13:02:51 -0400

    added more animation, fixed images, added sound

--- js/anim.js
+++ js/anim.js
@@ -1,15 +1,35 @@
 $(document).ready(function() {
-    $('.toggle').click(function() {
-        // $("#large-sound").toggleClass('large-sound');
-        // $("#medium-sound").toggleClass('medium-sound');
-        // $("#small-sound").toggleClass('small-sound');
-        // $("#yellow-spark").toggleClass('yellow-spark');
-        // $("#blue-spark").toggleClass('blue-spark');
-        // $("#pink-spark").toggleClass('pink-spark');
-        // alert('toggled');
-        $(this).css('display', 'none');
-        console.log('audio should trigger once toggle sound disappears');
-        console.log('guitar should be colorful and animation wiggle');
+    // $('.toggle').click(function() {
+    //     $(this).css('display', 'none');
+    //     console.log('audio should trigger once toggle sound disappears');
+    //     console.log('guitar should be colorful and animation wiggle');
+    // });
+    var sndPlaying = false;
+    var audioPlayerObj = document.getElementById("audioplayer");
+    $('.microphone1, .guitar-change').click(function() {
+        // alert("this is working");
+        if (sndPlaying == false) {
+            $('.microphone1').attr("src", "img/mic_anim.gif");
+            $('.guitar-change').attr("src", "img/guitarcolor.png");
+            $('.guitar1').addClass("wiggle");
+            $('.guitar2').addClass("wiggle2");
+            audioPlayerObj.play();
+            sndPlaying = true;
+        } else {
+            $('.microphone1').attr("src", "img/mic.png");
+            $('.guitar-change').attr("src", "img/guitar.png");
+            $('.guitar1').removeClass("wiggle");
+            $('.guitar2').removeClass("wiggle2");
+            audioPlayerObj.pause();
+            sndPlaying = false;
+        }
+
     });
+
+    // $('#microphone1').click(function() {
+    //     $(this).attr("src", "img/mic_anim.gif")
+    //     console.log('audio should trigger once toggle sound disappears');
+    //     console.log('guitar should be colorful and animation wiggle');
+    // });
 
 });
